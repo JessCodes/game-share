fa77890f6183b2e4c187a402efe28f3338fb80d6
Jen Young <jyoung488@gmail.com>
2017-01-05 14:44:32 -0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: "should match confirmation", if: :password
-  # has_many :games, foreign_key: :creator
   has_many :created_games, foreign_key: :creator, class_name: Game
 
   has_many :libraries
