f1dcea45c5de088b7b50de5e80aed66e5e3c34be
jyoung488 <jyoung488@gmail.com>
2017-01-06 02:45:02 -0800

    Add followship model and controller and allow users to follow each other

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,28 @@ class User < ApplicationRecord
   has_many :games, through: :libraries
 
   has_many :comments
+
+  has_many :active_followships, class_name:  "Followship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+
+  has_many :passive_relationships, class_name:  "Followship",
+                                 foreign_key: "followed_id",
+                                 dependent:   :destroy
+
+  has_many :following, through: :active_followships, source: :followed
+  has_many :followers, through: :passive_followships
+
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
