c2cc9f61d7dbf5ebd3ca26ea21462f986fd8f326
JessCodes <jessicafellison@gmail.com>
2017-01-05 16:38:21 -0800

    create comment migration

---
+++ db/migrate/20170106003728_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
+      t.integer :game_id
+
+      t.timestamps
+    end
+  end
+end
