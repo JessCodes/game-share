5000fc37f40fc65633452df63be33ad737146086
JessCodes <jessicafellison@gmail.com>
2017-01-05 16:58:56 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105224941) do
+ActiveRecord::Schema.define(version: 20170106003728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "game_mechanics", force: :cascade do |t|
     t.integer  "game_id"
