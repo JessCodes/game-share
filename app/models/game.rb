862d651e2c890dee46241b0c98de94e9c8c95df7
Jess Ellison <jessicafellison@gmail.com>
2017-01-06 11:48:19 -0800

    Update game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,10 +4,8 @@ class Game < ApplicationRecord
   has_many :libraries
   has_many :users, through: :libraries
 
-  #Game has many game mechanics
   has_many :game_mechanics
   has_many :mechanics, through: :game_mechanics
 
-  #Can upvote and downvote games
   has_many :votes, as: :voteable
 end
