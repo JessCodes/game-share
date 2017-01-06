f5db031a0dbeb92520ccda3d528450d4ef0e788b
Jess Ellison <jessicafellison@gmail.com>
2017-01-06 11:49:17 -0800

    Update 20170106065807_create_votes.rb

--- db/migrate/20170106065807_create_votes.rb
+++ db/migrate/20170106065807_create_votes.rb
@@ -1,9 +1,7 @@
 class CreateVotes < ActiveRecord::Migration[5.0]
-#Votes is polymorphic - comments and games are voteable
   def change
     create_table :votes do |t|
 
-      #voteable, NOT votable
       t.integer :voteable_id #id of the comment or game voted on
       t.string :voteable_type #"Comment" or "Game"
 
