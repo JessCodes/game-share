02a19aaae5392f5c8d88057a74cbcad80d2780cb
Jen Young <jyoung488@gmail.com>
2017-01-06 16:53:09 -0800

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,49 +26,6 @@ class CommentsController < ApplicationController
     redirect_to game_comment_path
   end
 
-  def upvote
-    @comment = Comment.find(params[:id])
-    #if vote exists, set it to 1
-    if @comment.vote
-      if @comment.vote.value = 1
-        #reset vote
-        @comment.vote.destroy
-        #else set it to 1
-      else
-        @comment.vote.value = 1
-      end
-    #else make a new vote with value 1
-  else
-    v = @comment.votes.new(value: 1)
-    v.user_id = current_user.id
-    v.save
-    end
-    redirect_to game_path(@comment.game)
-  end
-
-  # def downvote
-  #   @comment = Comment.find(params[:id])
-  #
-  #   #if vote exists, set it to -1
-  #   if @comment.vote
-  #     if @comment.vote.value = -1
-  #       #reset vote
-  #       @comment.vote.destroy
-  #       #else set it to -1
-  #     else
-  #       @comment.vote.value = -1
-  #       v.save
-  #     end
-  #
-  #   #else make a new vote with value 1
-  #   else
-  #     v = @comment.votes.new(value: 1)
-  #     v.user_id = current_user.id
-  #     v.save
-  #   end
-  #     redirect_to game_path(@comment.game)
-  # end
-
   private
 
   def comment_params
