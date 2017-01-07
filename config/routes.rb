aadb089c37ca80ee7ac79b51f5c28395e72a3c26
JessCodes <jessicafellison@gmail.com>
2017-01-06 16:48:14 -0800

    add mechanics route with functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'welcome#index'
+    
     resources :users
 
     resources :welcome, only: [:index]
@@ -10,8 +11,12 @@ Rails.application.routes.draw do
     resources :games do
       resources :comments, only: [:new, :create, :destroy]
     end
+
+    resources :mechanics, only: [:show]
+    
+    resources :followships, only: [:create, :destroy]
+
     get 'login' => 'sessions#new'
     get 'logout' => 'sessions#destroy'
 
-    resources :followships, only: [:create, :destroy]
 end
